e8f396bec1fe621971d8f1a4e1f5ce53f8f9737f
Fred <8153358+slackingfred@users.noreply.github.com>
2023-10-31 18:32:58 -0700

    Replicate

--- go-backend/cmd/replicate/main.go
+++ go-backend/cmd/replicate/main.go
@@ -1,4 +1,93 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+)
+
+type ReplicateURLs struct {
+	Cancel string `json:"cancel"`
+	Get    string `json:"get"`
+}
+
+type ReplicateStableDiffusionResponse struct {
+	ID     string          `json:"id"`
+	Input  json.RawMessage `json:"input"`
+	Output []string        `json:"output"`
+	Status string          `json:"status"`
+	URLs   ReplicateURLs   `json:"urls"`
+
+	CompletedAt time.Time       `json:"completed_at"`
+	CreatedAt   time.Time       `json:"created_at"`
+	StartedAt   time.Time       `json:"started_at"`
+	Error       json.RawMessage `json:"error"`
+	Logs        string          `json:"logs"`
+	Metrics     interface{}     `json:"metrics"`
+	Version     string          `json:"version"`
+}
+
+const (
+	apiEndpoint = "https://api.replicate.com/v1/predictions"
+)
+
 func main() {
+	// Set the OpenAI API key as an environment variable.
+	replicateToken := os.Getenv("REPLICATE_API_TOKEN")
+	if replicateToken == "" {
+		log.Fatal("Please set the REPLICATE_API_TOKEN environment variable.")
+	}
+
+	// Create a new Resty client.
+	client := resty.New()
+
+	// Set the OpenAI API key as a header.
+	client.SetHeader("Authorization", fmt.Sprintf("Token %s", replicateToken))
+
+	// Create a new request object.
+	req := client.R().
+		SetBody(map[string]interface{}{
+			"version": "ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
+			"input": map[string]interface{}{
+				"prompt": "An Asian boy celebrating Halloween",
+			},
+		}).
+		SetHeader("Content-Type", "application/json")
+
+	// Send the request and get the response.
+	resp, err := req.Post(apiEndpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Decode the JSON response.
+	var respBody ReplicateStableDiffusionResponse
+	//log.Print(string(resp.Body()))
+	err = json.Unmarshal(resp.Body(), &respBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Working... Cancel at", respBody.URLs.Cancel)
+
+	// Get the result in a loop.
+	for respBody.Status != "succeeded" {
+		time.Sleep(500 * time.Millisecond)
+		resp, err := client.R().Get(respBody.URLs.Get)
+		if err != nil {
+			log.Fatal(err)
+		}
+		respBody = ReplicateStableDiffusionResponse{}
+		err = json.Unmarshal(resp.Body(), &respBody)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Print(respBody.Logs)
+	}
+
+	// Print the generated text.
+	fmt.Println("Done! Get the image at", respBody.Output[0])
 }
