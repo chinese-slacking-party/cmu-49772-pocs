c97dc7b534b3778c72902a0287241724e98fdf9a
Fred <8153358+slackingfred@users.noreply.github.com>
2024-07-21 19:24:05 +0000

    Mock

--- go-backend/cmd/heartwatch/main.go
+++ go-backend/cmd/heartwatch/main.go
@@ -13,6 +13,11 @@ func main() {
 	r.Run(":8002")
 }
 
+type HeartRate struct {
+	Time int64   `json:"time"`
+	Data float64 `json:"data"`
+}
+
 func handlePing(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
@@ -21,6 +26,20 @@ func handlePing(c *gin.Context) {
 
 func handleReport(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"message": "success",
+		"hr_high": 130,
+		"hr_low":  65,
+		"hr_rest": 75,
+		"hr_data": []HeartRate{
+			{Time: 1600000000, Data: 80},
+			{Time: 1600000010, Data: 85},
+			{Time: 1600000020, Data: 90},
+			{Time: 1600000030, Data: 95},
+			{Time: 1600000040, Data: 100},
+			{Time: 1600000050, Data: 105},
+			{Time: 1600000060, Data: 110},
+			{Time: 1600000070, Data: 115},
+			{Time: 1600000080, Data: 120},
+			{Time: 1600000090, Data: 125},
+		},
 	})
 }
