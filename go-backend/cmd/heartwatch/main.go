3848edb8bb8a7bcb7062ebf99431adf53d6245e4
Fred <8153358+slackingfred@users.noreply.github.com>
2024-07-21 18:29:53 +0000

    Skeleton

--- go-backend/cmd/heartwatch/main.go
+++ go-backend/cmd/heartwatch/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+	r.GET("/ping", handlePing)
+	r.GET("/api/v1/hbs/report", handleReport)
+	r.Run(":8002")
+}
+
+func handlePing(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
+
+func handleReport(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+	})
 }
