87cb816c6e1bd43e6bf2ade3625ea1e3fa548e66
Fred <8153358+slackingfred@users.noreply.github.com>
2024-07-22 15:42:31 -0700

    10-20 system, 62 channels (still not all)

--- py-trainer/load00.py
+++ py-trainer/load00.py
@@ -26,12 +26,37 @@ def load_SEED_labels(fname):
     return mat_labels['label']
 
 
+def examine_SEED(fname):
+    '''
+    What, no metadata of any interest is there.
+    How can we know the mapping from 62 columns to the actual channels?
+    ----------------------------------------
+    PS C:\Users\bspub\go\src\github.com\chinese-slacking-party\cmu-49772-pocs\py-trainer> python .\load00.py
+    __header__
+    b'MATLAB 5.0 MAT-file, Platform: PCWIN64, Created on: Fri Nov 08 10:01:03 2013'
+    __version__
+    1.0
+    __globals__
+    []
+    Subject name: zjy
+    '''
+    mat_data_seed = sio.loadmat(fname)
+    first = True
+    for key in mat_data_seed.keys():
+        if '_eeg' in key:
+            if first:
+                print('Subject name:', key[0:key.index('_eeg')])
+                first = False
+            continue  # This time, we want to see the other keys (metadata)
+        print(key)
+        print(mat_data_seed[key])
+
+
 def test_subject1():
     data_dict = load_SEED(r'U:\SEED_EEG\Preprocessed_EEG\1_20131027.mat')
     data_frame = data_dict['djc_eeg1']
     time_stamps = pd.date_range(start='2013-10-27', periods=data_frame.shape[0], freq='5ms')
     print(data_frame)
-    print(time_stamps)
 
 
 def test_subject15():
@@ -40,4 +65,4 @@ def test_subject15():
 
 
 if __name__ == '__main__':
-    test_subject15()
+    examine_SEED(r'U:\SEED_EEG\Preprocessed_EEG\15_20131016.mat')
