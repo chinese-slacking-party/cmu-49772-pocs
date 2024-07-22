cdd837dc56ae915031eec9ee2b95349c3f64a8ae
Fred <8153358+slackingfred@users.noreply.github.com>
2024-07-22 15:59:40 -0700

    Got the mapping

--- py-trainer/load00.py
+++ py-trainer/load00.py
@@ -29,7 +29,6 @@ def load_SEED_labels(fname):
 def examine_SEED(fname):
     '''
     What, no metadata of any interest is there.
-    How can we know the mapping from 62 columns to the actual channels?
     ----------------------------------------
     PS C:\Users\bspub\go\src\github.com\chinese-slacking-party\cmu-49772-pocs\py-trainer> python .\load00.py
     __header__
@@ -56,6 +55,17 @@ def test_subject1():
     data_dict = load_SEED(r'U:\SEED_EEG\Preprocessed_EEG\1_20131027.mat')
     data_frame = data_dict['djc_eeg1']
     time_stamps = pd.date_range(start='2013-10-27', periods=data_frame.shape[0], freq='5ms')
+    '''
+    According to the Excel file, channel order is:
+    FP1 FPZ FP2 AF3 AF4 F7 F5 F3
+    F1 FZ F2 F4 F6 F8 FT7 FC5
+    FC3 FC1 FCZ FC2 FC4 FC6 FT8 T7
+    C5 C3 C1 CZ C2 C4 C6 T8
+    TP7 CP5 CP3 CP1 CPZ CP2 CP4 CP6
+    TP8 P7 P5 P3 P1 PZ P2 P4
+    P6 P8 PO7 PO5 PO3 POZ PO4 PO6
+    PO8 CB1 O1 OZ O2 CB2
+    '''
     print(data_frame)
 
 
