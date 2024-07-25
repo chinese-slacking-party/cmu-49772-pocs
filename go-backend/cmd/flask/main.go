3b5241cdfa751a041b693865243ca6ba7d465f75
Fred <8153358+slackingfred@users.noreply.github.com>
2024-07-25 02:09:55 +0000

    His API is not very good either (dismissable should be dismissed or read)

--- go-backend/cmd/flask/main.go
+++ go-backend/cmd/flask/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"log"
+	"math/rand"
+
+	"github.com/go-resty/resty/v2"
+)
+
+var (
+	client = resty.New()
+)
+
 // We are rather interacting with somebody else's Flask app
 func main() {
+	req := client.R().SetBody(map[string]interface{}{
+		"msg_id":      rand.Int63(),
+		"content":     "Heart rate too high at 186.57",
+		"dismissable": "no",
+	})
+
+	resp, err := req.Post("http://100.28.74.221:5000/upsert")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print(string(resp.Body()))
 }
