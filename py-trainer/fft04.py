ba2c94fd7eeb11e8a0fa1ae4c606ab759e6bcc9a
Fred <8153358+slackingfred@users.noreply.github.com>
2024-07-29 19:02:46 -0700

    get_waves

--- py-trainer/fft04.py
+++ py-trainer/fft04.py
@@ -15,6 +15,21 @@ def fft(nparray):
     return freqs, Y
 
 
+def get_waves(f, Y, ranges):
+    ret = []
+    for low, high in ranges:
+        low_idx = np.argmax(f >= low)
+        high_idx = np.argmax(f >= high)
+        if low_idx == 0 or high_idx == 0:
+            ret.append(0)
+            continue
+        ret.append(np.mean(Y[low_idx:high_idx]))
+    return ret
+
+
 if __name__ == '__main__':
     # The more samples, the narrower (more precise) the frequency bins
-    print(fft(np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])))
+    ret = fft(np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    print(ret)
+    #print(get_waves(*ret, [(4, 8), (8, 13), (13, 30)]))
+    print(get_waves(*ret, [(13, 30), (30, 75)]))
