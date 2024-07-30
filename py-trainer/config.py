f7506e8cb125b3b62bb5a03e5f27efeac809ace6
Fred <8153358+slackingfred@users.noreply.github.com>
2024-07-29 18:35:34 -0700

    Update config

--- py-trainer/config.py
+++ py-trainer/config.py
@@ -1,2 +1,3 @@
 SAMPLE_RATE = 200.0  # in hertz (ref: https://bcmi.sjtu.edu.cn/home/seed/seed-iv.html)
-OVERLAP = 0.0  # fraction of frame to overlap in range (0,1)
+OVERLAP = 0.5  # fraction of frame to overlap in range (0,1)
+FRAME_DURATION = 4.0  # in seconds
